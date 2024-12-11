59b14cfd1278783eb8ed530fe4596a570920b3a9
harsh <harsh.shah@bytebran.com>
2024-12-11 13:29:17 +0530

    Latest Changes

--- src/Components/Admin/Reports.jsx
+++ src/Components/Admin/Reports.jsx
@@ -386,6 +386,14 @@ export default function Reports() {
                             <td style={{ padding: "22px" }}> {t(val.direction)}</td>
                             <td className="table_edit2">
                               <button
+                                disabled={!val.recording_url}
+                                style={{
+                                  backgroundColor: !val.recording_url
+                                    ? "var(--main-forgot-color)"
+                                    : "var(--main-orange-color)",
+                                  border: "none",
+                                  opacity: !val.recording_url ? "0.5" : "",
+                                }}
                                 onClick={() => {
                                   handlelistner(val.recording_url, val.start_stamp);
                                 }}
