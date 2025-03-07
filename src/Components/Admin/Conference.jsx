4ae60c45fda301857d82c4a56ec9599e981d5444
EsaLahtinen <esa.lahtinen@contakti.com>
2025-03-07 09:49:50 +0200

    Update Conference.jsx

--- src/Components/Admin/Conference.jsx
+++ src/Components/Admin/Conference.jsx
@@ -429,7 +429,7 @@ export default function Conference() {
                     className="d-flex align-items-center justify-content-between"
                     onClick={() => sortingTable("assign_pstn_number")}
                   >
-                    <p className="mb-0">{t("Phone number")}</p>
+                    <p className="mb-0">{t("Phone Number")}</p>
                     {arrowShow("assign_pstn_number")}
                   </div>
                 </th>
