2e3a594afd6bad493e100dc33d36fdb2bae60d4d
EsaLahtinen <esa.lahtinen@contakti.com>
2025-03-07 09:50:50 +0200

    Update IVR.jsx

--- src/Components/Admin/IVR.jsx
+++ src/Components/Admin/IVR.jsx
@@ -583,7 +583,7 @@ export default function IVR() {
                     className="d-flex align-items-center justify-content-between"
                     onClick={() => sortingTable("assign_pstn_number")}
                   >
-                    <p className="mb-0">{t("Phone number")}</p>
+                    <p className="mb-0">{t("Phone Number")}</p>
                     {arrowShow("assign_pstn_number")}
                   </div>
                 </th>
