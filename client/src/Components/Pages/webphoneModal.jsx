8db55112c30257ad9ae53f323f13ce23d170d26e
EsaLahtinen <esa.lahtinen@contakti.com>
2025-03-28 17:25:15 +0200

    Update webphoneModal.jsx

--- client/src/Components/Pages/webphoneModal.jsx
+++ client/src/Components/Pages/webphoneModal.jsx
@@ -189,7 +189,7 @@ const ContactCardModal = ({ show, onHide, contactName, activeTab }) => {
                       }}
                     >
                       <div className="row ">
-                        <div className="col-9  Numberlength">
+                        <div className="col-12  Numberlength">
                           {contactName?.extension}
                         </div>
                       </div>
